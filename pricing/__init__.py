920828a8729ed02eb862579910f0c1659ae19a51
swords50001 <scott@truvanahealth.com>
2026-04-16 12:40:45 -0500

    Update exports in pricing package
    Removed PortalConfig and run_portal from exports.

--- pricing/__init__.py
+++ pricing/__init__.py
@@ -1,13 +1,10 @@
 """Pricing lookup package."""
 
 from .model import ClothingPriceModel, DEFAULT_DOMAINS, RemoteLookupError, SearchResult
-from .portal import PortalConfig, run as run_portal
 
 __all__ = [
     "ClothingPriceModel",
     "DEFAULT_DOMAINS",
     "RemoteLookupError",
     "SearchResult",
-    "PortalConfig",
-    "run_portal",
 ]
